2c2e93c1942467be94303c1ce497447bd8201049
georgie naegele <gnaegele5254@stu.d214.org>
2024-04-10 12:55:09 -0500

    removed intro code, created @State var named "number1"

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var number1: Int
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        Text("Hey Partner")
     }
 }
