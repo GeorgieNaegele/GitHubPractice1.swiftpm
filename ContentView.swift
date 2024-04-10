6fd6368129df21d08f5c8661c628a8e7bc79cae1
georgie naegele <gnaegele5254@stu.d214.org>
2024-04-10 13:10:49 -0500

    My parnter cant commit/push (error was screenshoted and sent to Mr. Brown) so we are both working off my laptop.

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var number1: Int
+    @State var number1 = 1
+    @State var number2 = 2
     
     var body: some View {
-        Text("Hey Partner")
+        
+        TextField("number1", value: $number1, format: .number)
+        
+        
+        TextField("number2", value: $number2, format: .number)
     }
 }
